796a52c98a38ec0d257bc54289cf051fa3c9d460
Feno2105 <mamyrandria2105@gmail.com>
2025-06-24 17:09:55 +0300

    go

--- bud/app/views/template2.php
+++ bud/app/views/template2.php
@@ -67,7 +67,7 @@ if (session_status() === PHP_SESSION_NONE) {
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">NiceAdmin</span>
+                <span class="d-none d-lg-block">Dolibarr</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
